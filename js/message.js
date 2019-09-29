7154f3a8d79e4860b9f5f524671158eed1543a9a
miqilin21 <994718917@qq.com>
2019-09-29 19:07:36 +0800

    修改一个小bug

--- js/message.js
+++ js/message.js
@@ -52,7 +52,7 @@
       })
     },
     bindEvents: function () {
-      this.form.addEventListener('submit', function (e) {
+      this.form.addEventListener('submit', (e) => {
         e.preventDefault()
         this.saveMessage()
       })
