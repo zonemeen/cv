99ec153abf669682e48cd573ff9fd98c4bb47b2c
miqilin21 <994718917@qq.com>
2019-09-29 14:22:06 +0800

    添加MVC的M

--- js/message.js
+++ js/message.js
@@ -1,18 +1,9 @@
 !function () {
   var view = document.querySelector('section.message')
 
-  var controller = {
-    view: null,
-    messageList: null,
-    init: function (view) {
-      this.view = view
-      this.messageList = view.querySelector('#messageList')
-      this.form = view.querySelector('form')
-      this.initAV()
-      this.loadMessages()
-      this.bindEvents()
-    },
-    initAV: function () {
+  var model = {
+    //获取数据
+    init: function () {
       var APP_ID = 'RGtCuRgKv4G4ezvLaEu9jGxL-gzGzoHsz'
       var APP_KEY = 'oivD4n5NaYjamGxCXrg9CCpN'
       AV.init({
@@ -20,9 +11,38 @@
         appKey: APP_KEY
       })
     },
-    loadMessages: function () {
+    fetch: function(){
       var query = new AV.Query('Message');
-      query.find().then((messages) => {
+      return query.find() // Promise 对象
+    },
+    //创建数据
+    save: function(name, content){
+      var Message = AV.Object.extend('Message');
+      var message = new Message();
+      message.set({
+        'content': content,
+        'name': name
+      });
+      return message.save() // Promise 对象
+    }
+  }
+
+  var controller = {
+    view: null,
+    model: null,
+    messageList: null,
+    init: function (view, model) {
+      this.view = view
+      this.model = model
+
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    loadMessages: function () {
+      this.model.fetch().then((messages) => {
         let array = messages.map((item) => item.attributes)
         array.forEach((item) => {
           let li = document.createElement('li')
@@ -41,13 +61,7 @@
       let myForm = this.form
       let content = myForm.querySelector('input[name=content]').value
       let name = myForm.querySelector('input[name=name]').value
-      var Message = AV.Object.extend('Message');
-      var message = new Message();
-      message.set({
-        'content': content,
-        'name': name
-      });
-      message.save().then(function (object) {
+      this.model.save(name, content).then(function (object) {
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name}: ${object.attributes.content}`
         let messageList = document.querySelector('#messageList')
@@ -58,6 +72,6 @@
       })
     }
   }
-  controller.init(view)
+  controller.init(view, model)
 }.call()
 
